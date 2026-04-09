822281098d542e81fe86eca842ad40d62fc64242
voidhidan13 <maeva.garcia13210@gmail.com>
2026-04-09 15:40:50 +0200

    P1C7

--- P1/P1C3/énoncé/main.py
+++ P1/P1C3/énoncé/main.py
@@ -1,1 +1,4 @@
-## Écrivez votre code ici !
+age = 27
+prénom = "maeva"
+formation = "admministration systeme et réseaux"
+print(f"Bonjour je suis {prénom} j'ai {age} ans je souhaite rentrer en {formation}")
