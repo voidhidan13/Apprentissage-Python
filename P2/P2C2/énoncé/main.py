fc43ac1c8a67ec8c434b89e3d54dc8af9ac45ff3
voidhidan13 <maeva.garcia13210@gmail.com>
2026-04-10 11:27:32 +0200

    Liste entier

--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,1 +1,17 @@
-# Ecrivez votre code ici !
+#L'utilisateur ecrits ses valeurs
+
+Nombres = input("Choisisez une serie de nombres séparés par des virgules")
+
+#On divise ses valeurs a chaque , 
+liste_nombres = Nombres.split(",")
+#On vérifie la liste
+print(liste_nombres)
+#On crée une liste vide pour stocker les entiers a chaque boucle
+liste_nombres_entiers = []
+#La boucle parcourt la liste est transforme la valeur en entier a chaque tour
+for nombres_entier in liste_nombres :
+    nombres_entier = int(nombres_entier)
+    #A chaque tour la boucle ajoute la valeur dans la nouvelle liste
+    liste_nombres_entiers.append(nombres_entier)
+
+print(liste_nombres_entiers)
