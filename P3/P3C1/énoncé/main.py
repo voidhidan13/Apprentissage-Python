e5f23c2a5dcdc22771095ce35c83178c9f72505c
voidhidan13 <maeva.garcia13210@gmail.com>
2026-04-16 14:22:15 +0200

    import

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,1 +1,10 @@
-# Ecrivez votre code ici !
+#Import des fonctions addition et multiplaction
+import operations
+
+# Appel focnction addition
+resultat_addition = operations.addition(3,5)
+print(f"Le résultat de l'addition est {resultat_addition}")
+
+# Appel fonction multiplication
+resultat_multiplication = operations.multiplication(8,2)
+print(f"Le resultat de la multiplication est {resultat_multiplication}")
--- P3/P3C1/énoncé/operations.py
+++ P3/P3C1/énoncé/operations.py
@@ -1,1 +1,10 @@
-# Écrivez votre code ici !
+# fonction addition 
+
+def addition(a,b) :
+    resultat = a + b 
+    return resultat
+
+# fonction multiplication
+def multiplication (a,b) :
+    resultat = a * b 
+    return resultat
